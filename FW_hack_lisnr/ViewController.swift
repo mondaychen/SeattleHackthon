451d85bfdb4fa5ad0b1f8c72ca752feb88cb15fa
mondaychen <monday.chen@gmail.com>
2016-07-15 18:43:19 -0400

    fade out buttons after unlike/dislike

--- FW_hack_lisnr/ViewController.swift
+++ FW_hack_lisnr/ViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 import Koloda
 
-private var numberOfCards: UInt = 5
+private var numberOfCards: UInt = 1
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var kolodaView: KolodaView!
+    @IBOutlet weak var likenessButtonsView: UIView!
     
-    private var dataSource: Array<UIImage> = {
+    private var imageSource: Array<UIImage> = {
         var array: Array<UIImage> = []
         for index in 0..<numberOfCards {
             array.append(UIImage(named: "Card_like_\(index + 1)")!)
@@ -53,11 +54,13 @@ class ViewController: UIViewController {
 extension ViewController: KolodaViewDelegate {
     
     func kolodaDidRunOutOfCards(koloda: KolodaView) {
-        dataSource.insert(UIImage(named: "Card_like_6")!, atIndex: kolodaView.currentCardIndex - 1)
-        let position = kolodaView.currentCardIndex
-        kolodaView.insertCardAtIndexRange(position...position, animated: true)
+//        dataSource.insert(UIImage(named: "Card_like_6")!, atIndex: kolodaView.currentCardIndex - 1)
+//        let position = kolodaView.currentCardIndex
+//        kolodaView.insertCardAtIndexRange(position...position, animated: true)
+        kolodaView.resetCurrentCardIndex()
+        likenessButtonsView.fadeOut(duration: 0.2)
     }
-    
+
     func koloda(koloda: KolodaView, didSelectCardAtIndex index: UInt) {
         UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
     }
@@ -67,16 +70,42 @@ extension ViewController: KolodaViewDelegate {
 extension ViewController: KolodaViewDataSource {
     
     func kolodaNumberOfCards(koloda:KolodaView) -> UInt {
-        return UInt(dataSource.count)
+        return UInt(imageSource.count)
     }
     
     func koloda(koloda: KolodaView, viewForCardAtIndex index: UInt) -> UIView {
-        return UIImageView(image: dataSource[Int(index)])
+        return UIImageView(image: imageSource[Int(index)])
     }
     
     func koloda(koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
         return NSBundle.mainBundle().loadNibNamed("OverlayView",
             owner: self, options: nil)[0] as? OverlayView
     }
+}
+
+extension UIView {
+    
+    /**
+     Fade in a view with a duration
+     
+     - parameter duration: custom animation duration
+     */
+    func fadeIn(duration duration: NSTimeInterval = 1.0) {
+        UIView.animateWithDuration(duration, animations: {
+            self.alpha = 1.0
+        })
+    }
+    
+    /**
+     Fade out a view with a duration
+     
+     - parameter duration: custom animation duration
+     */
+    func fadeOut(duration duration: NSTimeInterval = 1.0) {
+        UIView.animateWithDuration(duration, animations: {
+            self.alpha = 0.0
+        })
+    }
+    
 }
 
